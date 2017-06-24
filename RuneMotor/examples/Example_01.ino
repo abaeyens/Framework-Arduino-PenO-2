632b87a39b3a116b42dd1fdc381dc7d23754b750
abaeyens <abaeyens@users.noreply.github.com>
2017-06-24 14:52:34 +0200

    Add files via upload

--- RuneMotor/examples/Example_01.ino
+++ RuneMotor/examples/Example_01.ino
@@ -1,9 +1,54 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <RuneMotor.h>
 
+void setup() {
+  // Initiate the motor pins.
+  // You can change the pin numbers in the library file if necessary.
+  initMotors();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Let's start with going full speed forward!
+  // Full speed forward is 255. Using a higher value will not increase the speed.
+  setSpeedMotor1(255);  // Motor 1
+  setSpeedMotor2(255);  // Motor 2
 
+  // Now take some time to go forward
+  delay(1000);
+
+  // Better to stop, we might hit something!
+  stopMotors();
+
+  // And wait again.
+  delay(1000);
+
+  // Thanks to PWM, we can also change the speed gradually.
+  // A for-loop is used to go trough all the speeds.
+  // Increase speed from 0 to 255 (maximum).
+  for(int i=0; i<255; i++){
+    setSpeedMotor1(i);
+    setSpeedMotor2(i);
+    delay(10);
+  }
+  // Decrease speed from 255 to 0.
+  for(int i=256; i>0; i--){
+    setSpeedMotor1(i);
+    setSpeedMotor2(i);
+    delay(10);
+  }
+  // Stop the motors to be sure.
+  stopMotors();
+  delay(1000);
+
+  // It's also possible to ride backwards.
+  // Full speed backward is -255. Using a lower value will not increase the speed.
+  // We now use the value 128 which should give a speed of around 1/3 to 1/2 of the maximum speed.
+  setSpeedMotor1(-128);
+  setSpeedMotor2(-128);
+  delay(1000);
+
+  // Let's stop the demo.
+  stopMotors();
+
+  // And start again after five seconds.
+  delay(5000);
 }
